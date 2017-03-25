52fa19eef7f2b54b0106d21ad360ecf126f12ccc
Adrian Pothuaud <adrianpothuaud2@gmail.com>
2017-03-25 12:32:55 +0100

    solved errors + new controls on candidat view

--- public/js/controllers/CandidatCtrl.js
+++ public/js/controllers/CandidatCtrl.js
@@ -1,5 +1,11 @@
 angular.module('CandidatCtrl', []).controller('CandidatController', function($scope) {
 
 	$scope.msgWelcomeCandidat = 'Bienvenue Candidat ! Surveillez les évènements...';
+	$scope.profile_progressbar_valuenow = 70;
+	$scope.profile_progressbar_valuenow_string = "70";
+	$scope.profile_progressbar_class = "progress-bar progress-bar-success";
+	$scope.profile_progressbar_style = "width: 70%";
+	$scope.onGoingEventsCpt = 3;
+	$scope.upComingEventsCpt = 6;
 
 });
