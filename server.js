91bd117360227f6f01ef0c710080ca1429b99950
Adrian Pothuaud <adrianpothuaud2@gmail.com>
2017-03-25 14:43:09 +0100

    comment 100%

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-// modules =================================================
+// Main server script
+
+// Dependencies =================================================
 var express        = require('express');
 var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var session        = require('express-session');
+
 // configuration ===========================================
-// config files
 var db             = require('./config/db');
-
 var port = process.env.PORT || 8080; // set our port
-
 // parse various different custom JSON types as JSON
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-
 // static files on server
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
-
 // session management
 app.use(session({secret: "This is a secret"}));
 
+// db connection
 mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -34,4 +33,5 @@ db.once('open', function() {
     console.log('Magic happens on port ' + port); 			// shoutout to the user
 });
 
-exports = module.exports = app; 						// expose app
+// Export ======================
+exports = module.exports = app;
