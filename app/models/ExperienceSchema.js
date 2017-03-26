d45c9e0e31fda32b5a9466761a0360102adf4b45
Adrian Pothuaud <adrianpothuaud2@gmail.com>
2017-03-26 09:07:49 +0200

    db population

--- app/models/ExperienceSchema.js
+++ app/models/ExperienceSchema.js
@@ -5,41 +5,41 @@ var mongoose = require('mongoose');
 
 // Basic Schema with attributes and collection spec
 var ExperienceSchema = mongoose.Schema({
-	jobName : {type : String, default: ''},
-	enterpriseName : {type : String, default: ''},
-	dateBegin : {type : Date, default: undefined},
-    dateEnd : {type : Date, default: undefined}
-}, {collection: "experiences"});
+    jobName: { type: String, default: '' },
+    enterpriseName: { type: String, default: '' },
+    dateBegin: { type: Date, default: undefined },
+    dateEnd: { type: Date, default: undefined }
+}, { collection: "experiences" });
 
 // Schema's methods =================================
 // Getters ==========================================
-ExperienceSchema.methods.getJobName = function () {
+ExperienceSchema.methods.getJobName = function() {
     return this.jobName;
 }
-ExperienceSchema.methods.getEnterpriseName = function () {
+ExperienceSchema.methods.getEnterpriseName = function() {
     return this.enterpriseName;
 }
-ExperienceSchema.methods.getDateBegin = function () {
+ExperienceSchema.methods.getDateBegin = function() {
     return this.dateBegin;
 }
-ExperienceSchema.methods.getDateEnd = function () {
-    return this.dateEnd;
-}
-// Setters ==========================================
-ExperienceSchema.methods.setJobName = function (n) {
+ExperienceSchema.methods.getDateEnd = function() {
+        return this.dateEnd;
+    }
+    // Setters ==========================================
+ExperienceSchema.methods.setJobName = function(n) {
     this.jobName = n;
 }
-ExperienceSchema.methods.setEnterpriseName = function (n) {
+ExperienceSchema.methods.setEnterpriseName = function(n) {
     this.enterpriseName = n;
 }
-ExperienceSchema.methods.setDateBegin = function (n) {
+ExperienceSchema.methods.setDateBegin = function(n) {
     this.dateBegin = n;
 }
-ExperienceSchema.methods.setDateEnd = function (n) {
-    this.dateEnd = n;
-}
-// Facilities ===========================================
-ExperienceSchema.methods.isOnGoing = function () {
+ExperienceSchema.methods.setDateEnd = function(n) {
+        this.dateEnd = n;
+    }
+    // Facilities ===========================================
+ExperienceSchema.methods.isOnGoing = function() {
     var now = new Date();
     return (this.dateBegin <= now && now <= this.dateEnd);
 }
