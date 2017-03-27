9dc709a7b0eaa127570dad0465ebadf2374d1d2e
Adrian Pothuaud <adrianpothuaud2@gmail.com>
2017-03-27 02:35:31 +0200

    Candidat @ Recruteur page dynamic content

--- public/js/controllers/CandidatCtrl.js
+++ public/js/controllers/CandidatCtrl.js
@@ -2,45 +2,83 @@
 
 angular.module('CandidatCtrl', []).controller('CandidatController', function($scope, $http, $location) {
 
-    console.log($scope);
-
-    // Profile progress bar configuration
-
-    // Request the API with process.env['API_KEY']
-    // 1 : get candidat id by requesting the API /candidats/byemail/
-
-    // TEST _id = "58d7da87a43807c788d07e6a"
+    // Request the API to get user informations
     $scope.$watch($http.get("http://localhost:8080/api" +
             "onsiteactionskey4YHTE7" +
             "/candidats/" +
             $location.search().id)
         .then(function(response) {
-            console.log("Response arrived !");
-            console.log(response);
-            $scope.response = response;
+            // set $scope attribute with user informations
+            $scope.userResponse = response;
         }).then(function() {
-            $scope.$evalAsync(function(response) {
-                console.log('$evalAsync executed');
-                $scope.msgWelcomeCandidat = 'Bienvenue ' + $scope.response.data.firstname + ' ' + $scope.response.data.lastname + ' ! Surveillez les évènements...';
-                $scope.profile_progressbar_valuenow = 0;
-                if ($scope.response.data.firstname) $scope.profile_progressbar_valuenow += 20;
-                if ($scope.response.data.lastname) $scope.profile_progressbar_valuenow += 20;
-                if ($scope.response.data.email) $scope.profile_progressbar_valuenow += 20;
-                if ($scope.response.data.connaissances.length > 0) $scope.profile_progressbar_valuenow += 20;
-                if ($scope.response.data.experiences.length > 0) $scope.profile_progressbar_valuenow += 20;
-                $scope.profile_progressbar_valuenow_string = $scope.profile_progressbar_valuenow.toString();
-                $scope.profile_progressbar_style = "width: " + $scope.profile_progressbar_valuenow + "%";
-            });
+            // Request the API to get Events informations
+            $http.get("http://localhost:8080/api" +
+                    "onsiteactionskey4YHTE7" +
+                    "/events")
+                .then(function(response) {
+                    // set $scope attribute with events list
+                    $scope.eventsResponse = response;
+                })
+                .then(function() {
+                    $scope.$evalAsync(function() {
+                        // Welcome message configuration
+                        $scope.msgWelcomeCandidat = 'Bienvenue ' + $scope.userResponse.data.firstname + ' ' + $scope.userResponse.data.lastname + ' !';
+                        // Profile progress bar configuration
+                        // % value
+                        $scope.profile_progressbar_valuenow = 0;
+                        if ($scope.userResponse.data.firstname) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.lastname) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.email) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.connaissances.length > 0) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.experiences.length > 0) $scope.profile_progressbar_valuenow += 20;
+                        // % value to string
+                        $scope.profile_progressbar_valuenow_string = $scope.profile_progressbar_valuenow.toString();
+                        // progress bar width
+                        $scope.profile_progressbar_style = "width: " + $scope.profile_progressbar_valuenow + "%";
+                        // progress bar class
+                        $scope.profile_progressbar_class = "progress-bar progress-bar-danger";
+                        if ($scope.profile_progressbar_valuenow > 25) {
+                            if ($scope.profile_progressbar_valuenow > 50) {
+                                if ($scope.profile_progressbar_valuenow > 75) {
+                                    $scope.profile_progressbar_class = "progress-bar progress-bar-success";
+                                } else {
+                                    $scope.profile_progressbar_class = "progress-bar progress-bar-info";
+                                }
+                            } else {
+                                $scope.profile_progressbar_class = "progress-bar progress-bar-warning";
+                            }
+                        }
+
+                        // Accessible events configuration
+                        // initialize events ongoing/upcoming/passed
+                        $scope.onGoingEventsCpt = 0;
+                        $scope.upComingEventsCpt = 0;
+                        $scope.passedEventsCpt = 0;
+                        $scope.onGoingEvents = [];
+                        $scope.upComingEvents = [];
+                        $scope.passedEvents = [];
+                        // loop on events and set $scope
+                        var now = new Date();
+                        $scope.eventsResponse.data.forEach(function(element) {
+                            var beginD = new Date(element.dateBegin).getTime(),
+                                endD = new Date(element.dateEnd).getTime();
+                            // MongoDB -> String -> JavaScript Objects
+                            element.dateBegin = new Date(element.dateBegin);
+                            element.dateEnd = new Date(element.dateEnd);
+                            if (beginD <= now.getTime() && now.getTime() <= endD) {
+                                $scope.onGoingEventsCpt += 1;
+                                $scope.onGoingEvents = $scope.onGoingEvents.concat(element);
+                            } else if (now.getTime() <= beginD) {
+                                $scope.upComingEventsCpt += 1;
+                                $scope.upComingEvents = $scope.upComingEvents.concat(element);
+                            } else {
+                                $scope.passedEventsCpt += 1;
+                                $scope.passedEvents = $scope.passedEvents.concat(element);
+                            }
+                        }, this);
+
+                        // use events in ng-repeat on HTML template candidat.html
+                    });
+                })
         }));
-
-
-
-    // $scope.profile_progressbar_valuenow = 70;
-
-    $scope.profile_progressbar_class = "progress-bar progress-bar-success";
-
-    // Accessible events configuration
-    $scope.onGoingEventsCpt = 3;
-    $scope.upComingEventsCpt = 6;
-
 });
--- public/js/controllers/RecruteurCtrl.js
+++ public/js/controllers/RecruteurCtrl.js
@@ -1,7 +1,86 @@
 // View Controller for recruteur.html HTML Template
 
-angular.module('RecruteurCtrl', []).controller('RecruteurController', function($scope) {
+angular.module('RecruteurCtrl', []).controller('RecruteurController', function($scope, $http, $location) {
 
-	$scope.msgWelcomeRecruteur = 'Bienvenue Recruteur !';
+    // Request the API to get user informations
+    $scope.$watch($http.get("http://localhost:8080/api" +
+            "onsiteactionskey4YHTE7" +
+            "/recruteurs/" +
+            $location.search().id)
+        .then(function(response) {
+            // set $scope attribute with user informations
+            $scope.userResponse = response;
+        }).then(function() {
+            // Request the API to get Events informations
+            $http.get("http://localhost:8080/api" +
+                    "onsiteactionskey4YHTE7" +
+                    "/events")
+                .then(function(response) {
+                    // set $scope attribute with events list
+                    $scope.eventsResponse = response;
+                })
+                .then(function() {
+                    $scope.$evalAsync(function() {
+                        // Welcome message configuration
+                        $scope.msgWelcomeRecruteur = 'Bienvenue ' + $scope.userResponse.data.firstname + ' ' + $scope.userResponse.data.lastname + ' !';
+                        // Profile progress bar configuration
+                        // % value
+                        $scope.profile_progressbar_valuenow = 0;
+                        if ($scope.userResponse.data.firstname) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.lastname) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.email) $scope.profile_progressbar_valuenow += 20;
+                        if ($scope.userResponse.data.enterpriseName) $scope.profile_progressbar_valuenow += 40;
+                        // % value to string
+                        $scope.profile_progressbar_valuenow_string = $scope.profile_progressbar_valuenow.toString();
+                        // progress bar width
+                        $scope.profile_progressbar_style = "width: " + $scope.profile_progressbar_valuenow + "%";
+                        // progress bar class
+                        $scope.profile_progressbar_class = "progress-bar progress-bar-danger";
+                        if ($scope.profile_progressbar_valuenow > 25) {
+                            if ($scope.profile_progressbar_valuenow > 50) {
+                                if ($scope.profile_progressbar_valuenow > 75) {
+                                    $scope.profile_progressbar_class = "progress-bar progress-bar-success";
+                                } else {
+                                    $scope.profile_progressbar_class = "progress-bar progress-bar-info";
+                                }
+                            } else {
+                                $scope.profile_progressbar_class = "progress-bar progress-bar-warning";
+                            }
+                        }
 
+                        // Accessible events configuration
+                        // initialize events ongoing/upcoming/passed
+                        $scope.onGoingEventsCpt = 0;
+                        $scope.upComingEventsCpt = 0;
+                        $scope.passedEventsCpt = 0;
+                        $scope.onGoingEvents = [];
+                        $scope.upComingEvents = [];
+                        $scope.passedEvents = [];
+                        // loop on events and set $scope
+                        var now = new Date();
+                        $scope.eventsResponse.data.forEach(function(element) {
+                            // ToDo ===================
+                            // if enterprise name match
+                            // ========================
+                            var beginD = new Date(element.dateBegin).getTime(),
+                                endD = new Date(element.dateEnd).getTime();
+                            // MongoDB -> String -> JavaScript Objects
+                            element.dateBegin = new Date(element.dateBegin);
+                            element.dateEnd = new Date(element.dateEnd);
+                            if (beginD <= now.getTime() && now.getTime() <= endD) {
+                                $scope.onGoingEventsCpt += 1;
+                                $scope.onGoingEvents = $scope.onGoingEvents.concat(element);
+                            } else if (now.getTime() <= beginD) {
+                                $scope.upComingEventsCpt += 1;
+                                $scope.upComingEvents = $scope.upComingEvents.concat(element);
+                            } else {
+                                $scope.passedEventsCpt += 1;
+                                $scope.passedEvents = $scope.passedEvents.concat(element);
+                            }
+                        }, this);
+
+                        // use events in ng-repeat on HTML template candidat.html
+                    });
+                })
+        }));
 });
